acfea4effd38ed222fe0683e94063bec76fd48dc
sung-won95 <parksw1995@gmail.com>
2024-11-07 16:55:51 +0900

    first commit

---
+++ src/main/java/com/example/study2/user/controller/UserController.java
@@ -0,0 +1,45 @@
+package com.example.study2.user.controller;
+
+import com.example.study2.user.entity.User;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+public class UserController {
+
+    List<User> userList = new ArrayList<>();
+
+    @GetMapping("/user/{userId}")
+    public User readUser(@PathVariable Integer userId){
+
+
+        return this.userList.get(userId);
+    }
+
+    @GetMapping("/user")
+    public List<User> readUser1(){
+        return userList;
+    }
+
+    @PostMapping("/user")
+    public User createUser(@RequestBody User user){
+
+        this.userList.add(user);
+
+        return user;
+    }
+
+    @PutMapping("/user/{userId}")
+    public User updateUser(@RequestBody User user, @PathVariable Integer userId){
+        this.userList.remove(userId.intValue());
+        this.userList.add(userId,user);
+
+        return this.userList.get(userId.intValue());
+    }
+
+}
---
+++ src/main/java/com/example/study2/user/entity/User.java
@@ -0,0 +1,9 @@
+package com.example.study2.user.entity;
+
+import lombok.Data;
+
+@Data
+public class User {
+    String name;
+    String age;
+}
